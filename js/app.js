260f0d86bbd3fb07e83b96e043caf52acb54fd98
RiffatAkash <riffatakash@gmail.com>
2022-05-09 03:33:29 +0600

    commit fix

--- js/app.js
+++ js/app.js
@@ -119,9 +119,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
@@ -137,6 +137,7 @@ const showPosts = (posts) => {
 
   posts.forEach((post) => {
     const div = createPost(post);
+    // console.log(post.comments[0].user);
     productsContainer.appendChild(div);
   });
 };
